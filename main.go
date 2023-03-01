40fbea0d92d2699e6e05f24b92805077ed8dc8bd
jais marine <jaismarine@MacBook-Pro-jais.local>
2023-03-01 23:28:09 +0700

    feat: add color

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package mylogger
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func LogInfo(message string) {
+    colorGreen := "\033[32m"
+	fmt.Print(string(colorGreen))
 	log.Printf("INFO - %v", message)
 }
