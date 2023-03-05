5740fa8f24f6a65debca67adb4b6cfe247129e08
jais marine <jaismarine@MacBook-Pro-jais.local>
2023-03-05 17:36:13 +0700

    refactor: add date into logger

--- main.go
+++ main.go
@@ -2,25 +2,26 @@ package logger
 
 import (
 	"fmt"
+	"time"
 )
 
 func LogInfo(message string) {
     colorGreen := "\033[32m"
 	colorReset := "\033[0m"
-	info := fmt.Sprintf("INFO -  %v", message)
+	info := fmt.Sprintf("%v INFO -  %v", time.Now(), message)
 	fmt.Println(string(colorGreen), info, string(colorReset))
 }
 
 func LogWarn(message string) {
     colorYellow := "\033[33m"
 	colorReset := "\033[0m"
-	info := fmt.Sprintf("WARN -  %v", message)
+	info := fmt.Sprintf("%v WARN -  %v", time.Now(), message)
 	fmt.Println(string(colorYellow), info, string(colorReset))
 }
 
 func LogError(message string) {
 	colorRed := "\033[31m"
 	colorReset := "\033[0m"
-	info := fmt.Sprintf("ERROR -  %v", message)
+	info := fmt.Sprintf("%v ERROR -  %v", time.Now(), message)
 	fmt.Println(string(colorRed), info, string(colorReset))
 }
